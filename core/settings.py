81b1142bc2d8a9665ea124817b11f62f989f8a7e
ubuntu <matifikus1@gmail.com>
2021-03-29 23:45:51 +0200

    debug2

--- core/settings.py
+++ core/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 25
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
