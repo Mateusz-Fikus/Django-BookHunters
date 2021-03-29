3c5d792207d26429e24051a137d774301acebd15
ubuntu <matifikus1@gmail.com>
2021-03-30 01:06:46 +0200

    Dns

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '64.227.115.103', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '64.227.115.103', 'localhost', 'bookhuntersproject.xyz', 'www.bookhuntersproject.xyz']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -112,7 +112,7 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 25
+EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
